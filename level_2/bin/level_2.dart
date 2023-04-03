fa9971b8b9bbcc274b75019c587dcaa0bceeda68
Robert Felker <felker.robert@scub.net>
2023-04-03 14:14:16 +0200

    first commit

---
+++ level_2/bin/level_2.dart
@@ -0,0 +1,5 @@
+import 'package:level_2/level_2.dart' as level_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${level_2.calculate()}!');
+}
---
+++ level_2/test/level_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:level_2/level_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
